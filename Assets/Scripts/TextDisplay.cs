00a72a38bd2560ab84a0b07ea90d7b58ef19bc75
YanagiRemi <yanagiremi45755@gmail.com>
2023-06-08 12:33:34 +0900

    ヒントの文章変更

--- Assets/Scripts/TextDisplay.cs
+++ Assets/Scripts/TextDisplay.cs
@@ -18,10 +18,11 @@ public class TextDisplay : MonoBehaviour
     public Sprite fireNabe;
     public GameObject logBridge;
     public bool brickHint=false;
-    public float waitTime=2.0f;
+    public float waitTime=3.0f;
     public BurnStrawHouse BurnStrawHouse;
     public bool strawHint=false;
     public bool woodHint=false;
+    public bool startHint=false;
     public WoodHouse woodHouse;
     public BurnStrawHouse burnStrawHouse;
 
@@ -34,45 +35,54 @@ public class TextDisplay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!logStairs.activeSelf&&chimney.isBurning){
-            textComponent.text="ヒント:家の左側に階段を設置しよう";
+        if(!startHint){
+            textComponent.text="ヒント:まずはレンガの家の煙突に入れるようにしよう。窓からうかがった時、ブタが煙突の中に熱々の鍋を用意していたから注意が必要だ。。";
+            StartCoroutine(StartWait());
         }else if(bucket.GetComponent<SpriteRenderer>().sprite!=bucketWithWater && chimney.isBurning){
-            textComponent.text="ヒント:バケツに水を汲もう";
+            textComponent.text="ヒント:まずは煙突の中の熱々の鍋の火を消せるものを用意しよう";
+        }else if(!logStairs.activeSelf&&chimney.isBurning){
+            textComponent.text="ヒント:藁の家の左側に階段をつくって登れるようにしよう。";
         }else if(!woodenBridge2.activeSelf){
-            textComponent.text="ヒント:家と家の間を渡れる橋を作ろう。木の家の屋根の上に使えそうなものがあるね";
+            textComponent.text="ヒント:藁の家とレンガの家の間が渡れない。木の家の屋根の上に使えそうなものがあったはず。。";
         }else if(!chimney.isWatering&&chimney.isBurning){
             textComponent.text="ヒント:ブタが仕掛けた煙突の中の熱い鍋をどうにかしたいな。。";
         }else if(chimney.isWatering){
-            textComponent.text="煙突の中に水を入れている";
+            textComponent.text="煙突の中に水を入れている。";
         }else if(!chimney.isBurning && !chimney.hasEnterdBrickHouse){
-            textComponent.text="ヒント:煙突に入ろう";
+            textComponent.text="ヒント:煙突に入ろうか";
         }else if(!brickHint&&chimney.hasEnterdBrickHouse){
-            textComponent.text="ヒント:藁のブタが出てきた";
+            textComponent.text="ヒント:藁の家のブタは木の家に逃げた。ナイフと鉄のナベを家の中で見つけた。";
             StartCoroutine(Wait1());
         }else if(!logBridge.activeSelf && !ax.activeSelf){
-            textComponent.text="ヒント:川の上に橋を作って渡ろう。丸太で作れそうだ";
+            textComponent.text="ヒント:川の上が渡れないから橋を作ろう。さっきの丸太で作れそうだ。";
         }else if(!ax.activeSelf){
-            textComponent.text="ヒント:木に縛り付けられたオノのロープを切ろう";
+            textComponent.text="ヒント:木に縛り付けられたオノのロープを何かで切ろう。";
         }else if(!woodhouse.isBrokenHouse){
-            textComponent.text="ヒント:オノで木の家のドアを壊そう";
+            textComponent.text="ヒント:木の家のドアを壊したい。";
         }else if(!woodHouse.hasEnteredWoodHouse&&woodhouse.isBrokenHouse){
             textComponent.text="ヒント:木の家に入ろう";
         }else if(!woodHint&&woodHouse.hasEnteredWoodHouse){
-            textComponent.text="ヒント:木のブタが出てきた";
+            textComponent.text="ヒント:木の家のブタは藁の家に逃げた。ハンマーを家の中で見つけた。";
             StartCoroutine(Wait2());
         }else if(!brick.activeSelf){
-            textComponent.text="ヒント:レンガの家にヒビが入っている部分があるね。ハンマーで壊そう";
+            textComponent.text="ヒント:レンガの家にヒビが入っている部分があるね。叩いたらレンガが取れそう";
         }else if(nabe.GetComponent<SpriteRenderer>().sprite!=fireNabe){
-            textComponent.text="ヒント:藁の家は火をつけて壊そう。火は石と鉄で起こせそうだ。ナベは鉄製だ。";
+            textComponent.text="ヒント:藁の家は火をつけて壊そう。火は石と鉄で起こせそうだ。使えるものはないかな。";
         }else if(!burnStrawHouse.hasBurnedStrawHouse&&!burnStrawHouse.isStrawBurning){
-            textComponent.text="ヒント:ナベの火を藁の家に引火させよう。";
+            textComponent.text="ヒント:ナベに火がついた。ナベの火を藁の家に引火させよう。";
         }else if(burnStrawHouse.isStrawBurning){
-            textComponent.text="藁が燃えている。";
+            textComponent.text="藁が燃えている";
         }else if(!strawHint){
-            textComponent.text="三匹のブタが出てきた";
+            textComponent.text="やったー！ごちそうだ！いただきまーす！";
             StartCoroutine(Wait3());
         }
     }
+
+    IEnumerator StartWait(){
+        yield return new WaitForSeconds(waitTime);
+        startHint=true;
+    }
+
     IEnumerator Wait1(){
         yield return new WaitForSeconds(waitTime);
         brickHint=true;
